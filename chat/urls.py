5b78202cdf81071c6551117b88375a53407031da
An Bui <anhipchipchip@gmail.com>
2023-02-19 22:49:31 -0500

    finished all routes

--- chat/urls.py
+++ chat/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
    # path('', views.index, name='index'),
-   re_path(r'^api/chat$', views.chat_list)
+   re_path(r'^api/chat$', views.chat_list),
+   re_path(r'^api/chat/(?P<pk>[0-9]+)$', views.chat_detail)
 ]
