84984a4ec61efd44feb28864257c0b9dbd7022a7
An Bui <anhipchipchip@gmail.com>
2023-02-19 22:14:17 -0500

    post route

--- chat/serializers.py
+++ chat/serializers.py
@@ -4,4 +4,5 @@ from .models import Message
 class MessageSerializer(serializers.ModelSerializer):
   class Meta:
     model = Message
-    fields = '__all__'
+    # fields = '__all__'
+    fields = ('id', 'value', 'date', 'user', 'room', 'receiver')
--- chat/views.py
+++ chat/views.py
@@ -1,44 +1,46 @@
-<<<<<<< HEAD
-from django.http import JsonResponse
-from chat.models import Room
-
-def index(request):
-    rooms = Room.objects.all()
-    data = list(rooms.values())
-    return JsonResponse(data, safe=False)
-=======
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Message
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.shortcuts import get_object_or_404
-# from .serializers import MessageSerializer
+# from django.http import HttpResponse
+from django.http.response import JsonResponse
+# from .models import Message
+# from rest_framework.response import Response
+# from rest_framework import status
+# from django.shortcuts import get_object_or_404
+from chat.models import Message
+from chat.serializers import MessageSerializer
+from rest_framework.decorators import api_view
 
 def index(request):
   return HttpResponse('<h1> MY INDEX PAGE </h1>')
 
-class Message(APIView):
-
-  def get(self, request):
-    # Index Request
-    print(request)
-    # Get all messages from the chat table
+@api_view(['GET', 'POST'])
+def chat_list(request):
+  if (request.method == 'GET'):
     messages = Message.objects.all()
-    # Use serializer to format table data to JSON
-    data = MessageSerializer(messages, many=True).data
-    return Response(data)
+    messages_serializer = MessageSerializer(messages, many=True)
+    return JsonResponse(messages_serializer.data, safe=False)
+  elif request.method == 'POST':
+    chat_data = JSONParser
+
+
+# class Message(APIView):
+
+#   def get(self, request):
+#     # Index Request
+#     print(request)
+#     # Get all messages from the chat table
+#     messages = Message.objects.all()
+#     # Use serializer to format table data to JSON
+#     data = MessageSerializer(messages, many=True).data
+#     return Response(data)
   
-  def post(self, request):
-    # Post Request
-    print(request.data)
-    # format data for postgres
-    chat = MessageSerializer(data=request.data)
-    if chat.is_valid():
-      chat.save()
-      return Response(chat.data, status=status.HTTP_201_CREATED)
-    else: 
-      return Response(chat.errors, status=status.HTTP_400_BAD_REQUEST)
+#   def post(self, request):
+#     # Post Request
+#     print(request.data)
+#     # format data for postgres
+#     chat = MessageSerializer(data=request.data)
+#     if chat.is_valid():
+#       chat.save()
+#       return Response(chat.data, status=status.HTTP_201_CREATED)
+#     else: 
+#       return Response(chat.errors, status=status.HTTP_400_BAD_REQUEST)
     
->>>>>>> bf4c2610c2a987e0258c112c9c133d774a8dde18
