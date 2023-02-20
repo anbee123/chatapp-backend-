bf4c2610c2a987e0258c112c9c133d774a8dde18
abravebean <kevin@DESKTOP-GK03AKG.localdomain>
2023-02-19 20:16:09 -0500

    first commit

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Message
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+# from .serializers import MessageSerializer
 
 def index(request):
   return HttpResponse('<h1> MY INDEX PAGE </h1>')
+
+class Message(APIView):
+
+  def get(self, request):
+    # Index Request
+    print(request)
+    # Get all messages from the chat table
+    messages = Message.objects.all()
+    # Use serializer to format table data to JSON
+    data = MessageSerializer(messages, many=True).data
+    return Response(data)
+  
+  def post(self, request):
+    # Post Request
+    print(request.data)
+    # format data for postgres
+    chat = MessageSerializer(data=request.data)
+    if chat.is_valid():
+      chat.save()
+      return Response(chat.data, status=status.HTTP_201_CREATED)
+    else: 
+      return Response(chat.errors, status=status.HTTP_400_BAD_REQUEST)
+    
--- my_chatapp/settings.py
+++ my_chatapp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'chat'
 ]
 
@@ -76,8 +77,8 @@ WSGI_APPLICATION = 'my_chatapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'chat',
     }
 }
 
