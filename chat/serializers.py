f79678be4d74a79a1221f7495a754e79ad8e5c35
An Bui <anhipchipchip@gmail.com>
2023-02-21 23:17:17 -0500

    working crud

--- chat/serializers.py
+++ chat/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 
 from .models import Message
+# from .models import User
 class MessageSerializer(serializers.ModelSerializer):
   class Meta:
     model = Message
     # fields = '__all__'
     fields = ('id', 'value', 'date', 'user', 'room', 'receiver')
+
+# class UserSerializer(serializers.ModelSerializer):
+#   class Meta:
+#     model = User
+#     fields = ('id', 'name', 'email', 'password')
